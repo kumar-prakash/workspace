41363b333782f772f2b895f8a0525c741291885a
Prakash Kumar <prakash.kumar.2406@gmail.com>
2018-09-28 00:29:37 +0530

    Added DFS version of finding moother vertex

--- datastructure/graph/MotherVertex.java
+++ datastructure/graph/MotherVertex.java
@@ -44,6 +44,63 @@ public class MotherVertex {
         return mother;
     }
 
+    /**
+     * This method finds the mother vertex, there can be more than one mother vertex, 
+     * this will return any one of them;
+     * Approach: It runs DFS for every vertex and checks whether all other vertex are reachable or not.
+     * Note: This DFS is stack based instead of recursion.
+     * 
+     * @param list adjacency list
+     * @return  mother vertex if exist or return -1
+     */
+    public static int findMotherVertexDFS(LinkedList<Integer> [] list) {
+        int mother = -1;
+        if(list == null || list.length == 0) {
+            throw new IllegalArgumentException("Adjacency list cann't be null or empty.");
+        }
+        if(list.length == 1) {
+            return 1;
+        }
+
+        for(int i = 1; i < list.length; i++) {
+            boolean [] visited = new boolean[list.length];
+            Stack<Integer> stack = new Stack();
+            Stack<Iterator<Integer>> itrStack = new Stack();
+            stack.push(i);
+            itrStack.push(list[i].iterator());
+            visited[i] = true;
+            boolean isFirstStatement = true;
+            int visitedCount = 1;
+            start: 
+                while(!stack.isEmpty()) {
+                    if(isFirstStatement) {
+                        visitedCount++;    
+                    }
+                    while(itrStack.peek().hasNext()) {
+                        int vertex = itrStack.peek().next();
+                        if(!visited[vertex]) {
+                            visited[vertex] = true;
+                            stack.push(vertex);
+                            itrStack.push(list[vertex].iterator());
+                            isFirstStatement = true;
+                            
+                            continue start;
+                        }
+                    }
+                    while(!itrStack.isEmpty() && !itrStack.peek().hasNext()) {
+                        stack.pop();
+                        itrStack.pop();
+                        isFirstStatement = false;
+                    }   
+                }
+            if(visitedCount == list.length) {
+                mother = i;
+            }
+        }
+
+        return mother;
+    }
+
     private static void runTest(String [] data) {
         int motherVertex = findMotherVertex(GraphUtil.createAdjacencyList(data));
         if(motherVertex != -1) {
@@ -53,45 +110,80 @@ public class MotherVertex {
         }
     }
 
-    private static void test1() {
-        String [] data = {
-            "1=2,6",
-            "2=3",
-            "3=4,1,5",
-            "4=",
-            "5=",
-            "6="
-        };
+    private static void runTestDFS(String [] data) {
+        int motherVertex = findMotherVertexDFS(GraphUtil.createAdjacencyList(data));
+        if(motherVertex != -1) {
+            System.out.println("The mother vertex is: " + motherVertex);
+        } else {
+            System.out.println("Mother vertex does not exist");
+        }
+    }
 
-        runTest(data);
+    private static void test1() {
+        runTest(data1);
     }
 
     private static void test2() {
-        String [] data = {
-            "1=2,6",
-            "2=",
-            "3=2,1,4,5",
-            "4=",
-            "5=",
-            "6="
-        };
-
-        runTest(data);
+        runTest(data2);
     }
 
     private static void test3() {
-        String [] data = {
-            "1=2",
-            "2=",
-            "3=1",
-            "4=2"
-        };
-
-        runTest(data);
+        runTest(data3);
     }
-    public static void main(String... s) {
+
+    private static void testBFS() {
         test1();    
         test2();
         test3();
     }
+
+
+    private static void test1DFS() {
+        runTestDFS(data1);
+    }
+
+    private static void test2DFS() {
+        runTestDFS(data2);
+    }
+
+    private static void test3DFS() {
+        runTestDFS(data3);
+    }
+
+
+    private static void testDFS() {
+        test1DFS();
+        test2DFS();
+        test3DFS();
+    }
+    public static void main(String... s) {
+        testBFS();
+        testDFS();
+    }
+
+
+    private static String [] data1 = {
+        "1=2,6",
+        "2=3",
+        "3=4,1,5",
+        "4=",
+        "5=",
+        "6="
+    };
+
+    private static String [] data2 = {
+        "1=2,6",
+        "2=",
+        "3=2,1,4,5",
+        "4=",
+        "5=",
+        "6="
+    };
+
+    private static String [] data3 = {
+        "1=2",
+        "2=",
+        "3=1",
+        "4=2"
+    };
 }
